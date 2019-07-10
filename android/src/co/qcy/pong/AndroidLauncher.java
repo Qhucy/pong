a5f4883f1139ec784662b9262fb16bfd630e5508
Kucy Sijyo <businesstd6@gmail.com>
2019-07-09 23:59:40 -0700

    Syncing

---
+++ android/src/co/qcy/pong/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package co.qcy.pong;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import co.qcy.pong.Pong;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Pong(), config);
+	}
+}
---
+++ desktop/src/co/qcy/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,26 @@
+package co.qcy.pong.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import co.qcy.pong.Pong;
+
+public class DesktopLauncher
+{
+	
+	public static void main (String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pong(), config);
+		
+		config.title = "uwu";
+		config.width = Gdx.graphics.getWidth();
+		config.height = Gdx.graphics.getHeight();
+		config.fullscreen = true;
+		config.addIcon("icon_128.png", FileType.Internal);
+		config.addIcon("icon_32.png", FileType.Internal);
+		config.addIcon("icon_16.png", FileType.Internal);
+	}
+	
+}
